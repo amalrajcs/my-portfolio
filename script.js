22e63a9f28f7bc811824978b1e916363b911d482
amalrajcs <amalrajcs712@gmail.com>
2025-05-27 21:39:19 +0530

    Updated Code

--- script.js
+++ script.js
@@ -4,28 +4,3 @@ const toggle = document.getElementById('menu-toggle');
   toggle.addEventListener('click', () => {
     navItems.classList.toggle('show');
   });
-
-  // Cursor
-
-  const dot = document.querySelector('.cursor-dot');
-  const ring = document.querySelector('.cursor-ring');
-
-  let mouseX = 0, mouseY = 0;
-  let ringX = 0, ringY = 0;
-
-  document.addEventListener('mousemove', e => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-    dot.style.left = mouseX + 'px';
-    dot.style.top = mouseY + 'px';
-  });
-
-  function animate() {
-    ringX += (mouseX - ringX) / 8;
-    ringY += (mouseY - ringY) / 8;
-    ring.style.left = ringX + 'px';
-    ring.style.top = ringY + 'px';
-    requestAnimationFrame(animate);
-  }
-
-  animate();
